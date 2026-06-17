4a30af46aa9105946e1df151c058d5c7d3f932e8
AgustinaValverdi <agusvalverdi1@gmail.com>
2026-06-17 00:56:57 -0300

    agrego capa de persistencia

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/IPersistence.cs
@@ -0,0 +1,16 @@
+﻿using Dsw2026Ej15.Dsw2026Ej15.Domain;
+
+namespace Dsw2026Ej15.Dsw2026Ej15.Data
+{
+    public interface IPersistence
+    {
+        IReadOnlyCollection<Speciality> GetSpecialities();
+        Speciality? GetSpecialityById(Guid id);
+
+        IReadOnlyCollection<Doctor> GetDoctors();
+        Doctor? GetDoctorById(Guid id);
+
+        void AddDoctor(Doctor doctor);
+        void UpdateDoctor(Doctor doctor);
+    }
+}
